9a769d3b07ad736bce95f651eb88fd586498dcc4
Krishna K <krish1826s@gmail.com>
2024-10-22 09:37:26 +0530

    Time: 126 ms (19.64%), Space: 49 MB (71.86%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countPrimes(int n) {
+        int c=0;
+        boolean []arr=new boolean[n];
+        Arrays.fill(arr,true);
+        for(int i=2;i<n;i++){
+            if(arr[i]){
+                c++;
+               // System.out.println(""+i);
+                for(int j=1;j*i<n;j++){
+                    arr[j*i]=false;
+                    
+                            }
+                                        }
+        }
+        
+        return c;
+    }
+    }
