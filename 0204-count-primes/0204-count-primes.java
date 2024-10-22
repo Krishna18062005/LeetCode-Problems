47e81c13508c781224a3858ed3da2c96479746ad
Krishna K <krish1826s@gmail.com>
2024-10-22 10:08:11 +0530

    Time: 138 ms (16.86%), Space: 49.4 MB (32.73%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -3,14 +3,14 @@ class Solution {
         int c=0;
         boolean []arr=new boolean[n];
         Arrays.fill(arr,true);
+        for(int i=2;i*2<n;i++) arr[2*i]=false;
         for(int i=2;i<n;i++){
             if(arr[i]){
                 c++;
                 for(int j=1;j*i<n;j++){
                     arr[j*i]=false;
-                    
-                            }
-                                        }
+                }
+            }
         }
         
         return c;
