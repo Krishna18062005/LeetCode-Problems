3a1f05ff3d1e22826008dfb9a2857bfc2d5ce632
Krishna K <krish1826s@gmail.com>
2024-10-22 09:49:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -2,15 +2,20 @@ class Solution {
     public int countPrimes(int n) {
         int c=0;
         boolean []arr=new boolean[n];
-        Arrays.fill(arr,true);
+        if(n<=2) return 0; 
+        arr[2]=true;
+        for(int i=3;i<=n;i+=2) arr[i]=true;
         for(int i=2;i<n;i++){
             if(arr[i]){
                 c++;
+              // System.out.println(""+i);
                 for(int j=1;j*i<n;j++){
                     arr[j*i]=false;
-                    }
-                }
+                    
+                            }
+                                        }
         }
-       return c;
+        
+        return c;
     }
     }
