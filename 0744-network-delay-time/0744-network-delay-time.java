084337a5e5dfc0ecd21ca658830bb85767b66969
Krishna K <krish1826s@gmail.com>
2024-11-04 16:02:03 +0530

    Time: 15 ms (56.14%), Space: 48.6 MB (46.28%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -5,7 +5,8 @@ class Solution {
             dist[i] = Integer.MAX_VALUE;
         }
         dist[k] = 0;
-        for(int i=0; i<n-1; i++) {
+        int i=0;
+        for(; i++<n-1; ) {
             for(int j=0; j<times.length; j++) {
                 if(dist[times[j][0]]!=Integer.MAX_VALUE) {
                     dist[times[j][1]] = Math.min(dist[times[j][0]]+times[j][2], dist[times[j][1]]);
@@ -13,12 +14,10 @@ class Solution {
             }
         }
         int max = 0;
-        for(int i=0; i+1<dist.length; i++) {
+        for(i=0; i+1<dist.length; i++) {
             max = Math.max(max,dist[i+1]);
         }
-        if(max == Integer.MAX_VALUE) {
-            max = -1;
-        }
-        return max;
+        
+        return max==Integer.MAX_VALUE?-1:max;
     }
 }
