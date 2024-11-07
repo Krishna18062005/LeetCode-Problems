9804bde97fb9e49cec78bcc966cefc37c50bd614
Krishna K <krish1826s@gmail.com>
2024-11-07 10:24:13 +0530

    Time: 18 ms (95.24%), Space: 56.9 MB (19.52%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.java
@@ -2,8 +2,8 @@ class Solution {
     public int largestCombination(int[] candidates) {
         int[] arr=new int[32];
         int ans=0;
-        for(int k=0;k<candidates.length;k++){
-            int i=candidates[k];
+        for(int i:candidates){
+           
             for(int j=0;j<32;j++){
                 
                 arr[j]+=(i&1);
