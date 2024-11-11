6c79a3c6c30053ac1b5f4234f72d30b326707ab8
Krishna K <krish1826s@gmail.com>
2024-11-11 15:31:12 +0530

    Time: 8 ms (98.71%), Space: 55.1 MB (79.11%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.java
@@ -0,0 +1,32 @@
+class Solution{
+class TrieNode {
+        boolean isEnd;
+        TrieNode[] tns = new TrieNode[26];
+    }
+    TrieNode root = new TrieNode();
+    void add(String s) {
+        TrieNode p = root;
+        for (int i = 0; i < s.length(); i++) {
+            int u = s.charAt(i) - 'a';
+            if (p.tns[u] == null) p.tns[u] = new TrieNode();
+            p = p.tns[u];
+        }
+        p.isEnd = true;
+    }
+    String query(String s) {
+        TrieNode p = root;
+        for (int i = 0; i < s.length(); i++) {
+            int u = s.charAt(i) - 'a';
+            if (p.tns[u] == null) break;
+            if (p.tns[u].isEnd) return s.substring(0, i + 1);
+            p = p.tns[u];
+        }
+        return s;
+    }
+    public String replaceWords(List<String> ds, String s) {
+        for (String str : ds) add(str);
+        StringBuilder sb = new StringBuilder();
+        for (String str : s.split(" ")) sb.append(query(str)).append(" ");
+        return sb.substring(0, sb.length() - 1);
+    }
+}
