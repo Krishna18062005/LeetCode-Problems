353591d5658841a590b6561c9a5101913ae0d54b
Krishna K <krish1826s@gmail.com>
2024-11-24 10:27:16 +0530

    Time: 239 ms (33.33%), Space: 75.9 MB (100%) - LeetHub

--- 3595-rearrange-k-substrings-to-form-target-string/3595-rearrange-k-substrings-to-form-target-string.java
+++ 3595-rearrange-k-substrings-to-form-target-string/3595-rearrange-k-substrings-to-form-target-string.java
@@ -1,24 +1,36 @@
 class Solution {
     public boolean isPossibleToRearrange(String s, String t, int k) {
+        // int d=s.length()/k;
+        // if(s.equals(t)) return true;
+        // HashMap<String,Integer> map=new HashMap<>();
+        // for(int i=0;i<s.length();i+=d){
+        //     String req=s.substring(i,i+d);
+        //     if(map.containsKey(req)){
+        //         map.put(req,map.get(req)+1);
+        //     }
+        //     else
+        //     map.put(s.substring(i,i+d),1);
+        // }
+        // for(int i=0;i<t.length();i+=d){
+        //     String tar=t.substring(i,i+d);
+        //     if(!map.containsKey(tar)) return false;
+        //     else if(map.get(tar)<1) return false;
+        //     else{
+        //         map.put(tar,map.get(tar)-1);
+        //     }
+        // }
+        // return true;
         int d=s.length()/k;
-        if(s.equals(t)) return true;
-        HashMap<String,Integer> map=new HashMap<>();
+        String sA[]=new String[k];
+        String tA[]=new String[k];
+        int ind=0;
         for(int i=0;i<s.length();i+=d){
-            String req=s.substring(i,i+d);
-            if(map.containsKey(req)){
-                map.put(req,map.get(req)+1);
-            }
-            else
-            map.put(s.substring(i,i+d),1);
+            sA[ind]=s.substring(i,i+d);
+            tA[ind]=t.substring(i,i+d);
+            ++ind;
         }
-        for(int i=0;i<t.length();i+=d){
-            String tar=t.substring(i,i+d);
-            if(!map.containsKey(tar)) return false;
-            else if(map.get(tar)<1) return false;
-            else{
-                map.put(tar,map.get(tar)-1);
-            }
-        }
-        return true;
+        Arrays.sort(sA);
+        Arrays.sort(tA);
+        return Arrays.equals(sA,tA);
     }
 }
