57425d6cddca396694718d9cb224bae7b66b37de
Krishna K <krish1826s@gmail.com>
2024-12-09 21:32:18 +0530

    Time: 0 ms (100%), Space: 12.3 MB (9.91%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        return str(int(num1)*int(num2))
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        
