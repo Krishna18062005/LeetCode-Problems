6aa1c896548dc086992c18fe1d213b9d5a954cc4
Krishna K <krish1826s@gmail.com>
2024-12-12 11:41:28 +0530

    Time: 135 ms (5.76%), Space: 41.9 MB (94.95%) - LeetHub

---
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        for(int i=0;i<k;i++){
+            int maxi=0;
+            int max=gifts[0];
+            for(int j=1;j<gifts.length;j++){
+                if(max<gifts[j]){
+                    maxi=j;
+                    max=gifts[j];
+                }
+            }
+            gifts[maxi]=(int)Math.pow(gifts[maxi],0.5);
+        }
+        long s=0;
+        for(int i:gifts) s+=(long)i;
+        return s;
+          }
+}
