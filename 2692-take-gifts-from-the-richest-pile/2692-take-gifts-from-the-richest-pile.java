36c5ab07b6e591a5ea13a33584a4e2940139263c
Krishna K <krish1826s@gmail.com>
2024-12-12 11:45:56 +0530

    Time: 6 ms (65.07%), Space: 42.6 MB (30.16%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -1,18 +1,21 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
-        for(int i=0;i<k;i++){
-            int maxi=0;
-            int max=gifts[0];
-            for(int j=1;j<gifts.length;j++){
-                if(max<gifts[j]){
-                    maxi=j;
-                    max=gifts[j];
-                }
-            }
-            gifts[maxi]=(int)Math.sqrt(gifts[maxi]);
-        }
+        // for(int i=0;i<k;i++){
+        //     int maxi=0;
+        //     int max=gifts[0];
+        //     for(int j=1;j<gifts.length;j++){
+        //         if(max<gifts[j]){
+        //             maxi=j;
+        //             max=gifts[j];
+        //         }
+        //     }
+        //     gifts[maxi]=(int)Math.sqrt(gifts[maxi]);
+        // }
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+        for(int i:gifts) pq.offer(i);
+        while(k-->0) pq.add((int)Math.sqrt(pq.poll()));
         long s=0;
-        for(int i:gifts) s+=(long)i;
+        while(!pq.isEmpty()) s+=(long)pq.poll();
         return s;
           }
 }
