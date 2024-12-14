132aff3a5ea728827e5dd247e4d73ec93fdce844
Krishna K <krish1826s@gmail.com>
2024-12-14 21:11:29 +0530

    Time: 453 ms (11.57%), Space: 65.9 MB (84.67%) - LeetHub

--- 3066-create-a-new-column/3066-create-a-new-column.py
+++ 3066-create-a-new-column/3066-create-a-new-column.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
-    df=pd.DataFrame(employees,columns=['name','salary'])
-    df['bonus']=employees['salary']*2
-    return df
+
+    employees['bonus']=employees['salary']*2
+    return employees
