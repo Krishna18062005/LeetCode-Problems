766ccc4b60f18bd91b4cb5229529dccb29710979
Krishna K <krish1826s@gmail.com>
2024-12-23 08:06:57 +0530

    Time: 2 ms (87.8%), Space: 40.8 MB (8.41%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
         int s=0;
-        for(int i=1;i<num;i++){
-            if(num%i==0) s+=i;
+        for(int i=1;i<=Math.sqrt(num);i++){
+            if(num%i==0) {s+=i;s+=(num/i);}
         }
-        return s==num;
+        s-=num;
+        return s==num&&num!=1;
     }
 }
