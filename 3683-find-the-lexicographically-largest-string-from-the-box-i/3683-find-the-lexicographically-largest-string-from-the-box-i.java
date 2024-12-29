af022a6bd99c7cef9ee09d3efed0662e312945b9
Krishna K <krish1826s@gmail.com>
2024-12-29 13:01:12 +0530

    Time: 13 ms (100%), Space: 45.5 MB (100%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.java
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.java
@@ -1,3 +1,31 @@
+// class Solution {
+//     public String answerString(String word, int numFriends) {
+//         if(numFriends==1) return word;
+//         int len=word.length()-numFriends+1;
+//         String ans="";
+//         int maxInd=0;
+//         char ch=word.charAt(0);
+//         ArrayList<Integer> ls=new ArrayList<>();
+//         for(int i=0;i<word.length();i++){
+//             if(ch<word.charAt(i)) {
+//                 ch=word.charAt(i);
+//                 maxInd=i;
+//             }
+//         }
+//         for(int i=0;i<word.length();i++){
+//             if(ch==word.charAt(i)) ls.add(word.length()-i);
+//         }
+//         ls.add(len);
+        
+//         for(int le:ls){
+//             if(le>len){continue;}
+//         for(int i=0;i+le<=word.length();i++){
+//             if(ans.compareTo(word.substring(i,i+le))<0) ans=word.substring(i,i+le);
+//         }}
+//         return ans;
+//     }
+// }
+
 class Solution {
     public String answerString(String word, int numFriends) {
         if(numFriends==1) return word;
