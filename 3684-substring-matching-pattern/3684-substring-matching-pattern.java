b4784612e589cecb93121ff869758151595225e1
Krishna K <krish1826s@gmail.com>
2025-01-04 21:39:50 +0530

    Time: 2 ms (50%), Space: 43.1 MB (50%) - LeetHub

---
+++ 3684-substring-matching-pattern/3684-substring-matching-pattern.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasMatch(String s, String p) {
+        String[] parts = p.split("\\*", 2);
+        String pre = parts[0];
+        String suf = parts[1];
+        if(!s.contains(pre)) return false;
+        if(!s.contains(suf)) return false;
+        int i=s.indexOf(pre);
+        int j=s.indexOf(suf,i+pre.length());
+        if(suf.equals("")) i=0;
+        if(pre.equals("")) j=0;
+        if(i==-1||j==-1) return false;
+        return true;
+    }
+}
