53da6e0f2f94c1fd5fce0f82a4136cdbb858616a
Krishna K <krish1826s@gmail.com>
2025-01-19 09:19:23 +0530

    Time: 60 ms (100%), Space: 57.3 MB (0%) - LeetHub

--- 3712-minimum-cost-to-make-arrays-identical/3712-minimum-cost-to-make-arrays-identical.java
+++ 3712-minimum-cost-to-make-arrays-identical/3712-minimum-cost-to-make-arrays-identical.java
@@ -1,21 +1,18 @@
 class Solution {
     public long minCost(int[] arr, int[] brr, long k) {
-        int n = arr.length;
-
-        long woSort = 0;    // without sorting answer
-        for (int i = 0; i < n; i++) {
-            long d = Math.abs(arr[i] - brr[i]);
-            woSort += d;
-        }
-
-        Arrays.sort(arr);
-        Arrays.sort(brr);
-        long afterSort = k;    // after sorting answer
-        for (int i = 0; i < n; i++) {
-            long d = Math.abs(arr[i] - brr[i]);
-            afterSort += d;
+        long curr1 = 0, curr2 = k;
+        
+        for (int i = 0; i < arr.length; i++) {
+            curr1 += Math.abs(brr[i] - arr[i]);
         }
         
-        return Math.min(woSort, afterSort);
+        Arrays.sort(arr);
+        Arrays.sort(brr);
+        
+        for (int i = 0; i < arr.length; i++) {
+            curr2 += Math.abs(brr[i] - arr[i]);
+        }
+        
+        return Math.min(curr1, curr2);
     }
 }
