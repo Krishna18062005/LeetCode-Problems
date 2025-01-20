53c9cfbe115be5b61fe007a929fab38f5bdc2231
Krishna K <krish1826s@gmail.com>
2025-01-20 22:03:30 +0530

    Time: 22 ms (80.6%), Space: 65.7 MB (74.63%) - LeetHub

---
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int numRows = mat.length;
+        int numCols = mat[0].length;
+        int[] rowCount = new int[numRows];
+        int[] colCount = new int[numCols];
+        Map<Integer, int[]> numToPos = new HashMap<>();
+
+        // Create a map to store the position of each number in the matrix
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                int value = mat[row][col];
+                numToPos.put(value, new int[] { row, col });
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+            int[] pos = numToPos.get(num);
+            int row = pos[0];
+            int col = pos[1];
+
+            // Increment the count for the corresponding row and column
+            rowCount[row]++;
+            colCount[col]++;
+
+            // Check if the row or column is completely painted
+            if (rowCount[row] == numCols || colCount[col] == numRows) {
+                return i;
+            }
+        }
+
+        return -1; // This line will never be reached as per the problem statement
+    }
+}
