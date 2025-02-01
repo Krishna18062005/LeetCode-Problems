9c5f158172b82eb2a8a8b90767a1fbbd22f9ddc0
Krishna K <krish1826s@gmail.com>
2025-02-01 21:37:54 +0530

    Time: 5 ms (0%), Space: 43.3 MB (0%) - LeetHub

---
+++ 3736-find-valid-pair-of-adjacent-digits-in-string/3736-find-valid-pair-of-adjacent-digits-in-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String findValidPair(String s) {
+        int[] fre=new int[10];
+        for(int i=0;i<s.length();i++){
+            fre[s.charAt(i)-'0']++;
+        }
+        for(int i=1;i<s.length();i++){
+            int fs=s.charAt(i-1);
+            int sd=s.charAt(i);
+            if(fs!=sd&&fre[fs-'0']==fs-'0'&&fre[sd-'0']==sd-'0'){
+                return s.charAt(i-1)+""+s.charAt(i);                
+            }
+        }
+        return "";
+    }
+}
