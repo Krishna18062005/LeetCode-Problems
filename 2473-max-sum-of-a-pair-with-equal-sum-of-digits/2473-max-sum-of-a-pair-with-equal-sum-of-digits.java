52902d21d5245cbf03f41a0c7e7b80e98968a363
Krishna K <krish1826s@gmail.com>
2025-02-12 12:26:35 +0530

    Time: 18 ms (100%), Space: 55.3 MB (94.9%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,7 +1,6 @@
 class Solution {
     public int maximumSum(int[] nums) {
-        int ans=-1;
-       
+        int ans=-1;       
         int[] ar=new int[82];
         for(int i:nums){
             int s=0;
@@ -10,15 +9,13 @@ class Solution {
                 s+=t%10;
                 t/=10;
             }
-            if(ar[s]==0){
-                
+            if(ar[s]==0){                
                 ar[s]=i;
-
             }
             else{
                 ans=Math.max(i+ar[s],ans);
-                int mxx=Math.max(i,ar[s]);
-                ar[s]=mxx;
+                if(ar[s]<i)
+                ar[s]=i;
             }
         }
        
