ddab5d6293d78bf4794646d1ff912b07687317a8
Krishna K <krish1826s@gmail.com>
2025-02-13 10:39:43 +0530

    Time: 485 ms (55.04%), Space: 68.5 MB (45.7%) - LeetHub

---
+++ 3760-assign-elements-to-groups-with-constraints/3760-assign-elements-to-groups-with-constraints.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] assignElements(int[] groups, int[] elements) {
+        int [] ans=new int[groups.length];
+        Arrays.fill(ans,-1);
+        HashMap<Integer,Integer> el=new HashMap<>();
+        int max=0;
+        int ind=0;
+        for(int i:elements){
+            if(!el.containsKey(i))
+            el.put(i,ind);
+            ind++;
+        }
+        
+        ind=0;
+        for(int i:groups){
+            int an=Integer.MAX_VALUE;
+            boolean ch=false;
+         for(int j=1;j*j<=i;j++){
+            if(i%j==0){
+                if(el.containsKey(j)){
+                    ch=true;
+                    an=Math.min(el.get(j),an);
+                }
+                if(el.containsKey(i/j)){
+                    ch=true;
+                    an=Math.min(el.get(i/j),an);
+                }
+            }
+
+         }
+         if(ch)
+         ans[ind]=an;
+         ind++;
+        }
+        
+        
+        return ans;
+    }
+}
