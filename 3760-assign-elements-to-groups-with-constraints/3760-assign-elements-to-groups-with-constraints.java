8209e8bb44d9623efa4d1098218e378e00f58aee
Krishna K <krish1826s@gmail.com>
2025-02-13 10:40:57 +0530

    Time: 494 ms (46.47%), Space: 68.4 MB (45.7%) - LeetHub

--- 3760-assign-elements-to-groups-with-constraints/3760-assign-elements-to-groups-with-constraints.java
+++ 3760-assign-elements-to-groups-with-constraints/3760-assign-elements-to-groups-with-constraints.java
@@ -28,8 +28,8 @@ class Solution {
             }
 
          }
-         if(ch)
-         ans[ind]=an;
+         
+         ans[ind]=(an==Integer.MAX_VALUE)?-1:an;
          ind++;
         }
         
