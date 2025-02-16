ef59bb38a6cc2550c8d991e0d4ddc5b8a0177164
Krishna K <krish1826s@gmail.com>
2025-02-16 14:43:45 +0530

    Time: 1 ms (100%), Space: 42.4 MB (100%) - LeetHub

--- 3709-find-special-substring-of-length-k/3709-find-special-substring-of-length-k.java
+++ 3709-find-special-substring-of-length-k/3709-find-special-substring-of-length-k.java
@@ -3,12 +3,15 @@ class Solution {
        
         for(int i=0;i<=s.length()-k;i++){
             if(i==-1||i==0||s.charAt(i-1)!=s.charAt(i)){
-            Set<Character> set=new HashSet<>();
-            for(int j=i;j<i+k;j++){
-                set.add(s.charAt(j));
+            char fs=s.charAt(i);
+            int j=i;
+            for(j=i;j<i+k;j++){
+                if(fs!=s.charAt(j)){
+                    break;
+                }
             }
-            System.out.println(set);
-            if(set.size()==1&&(i+k>=s.length()||s.charAt(i+k)!=s.charAt(i+k-1))){
+            
+            if(j==i+k&&(i+k>=s.length()||s.charAt(i+k)!=s.charAt(i+k-1))){
                 return true;
             }}
         }
