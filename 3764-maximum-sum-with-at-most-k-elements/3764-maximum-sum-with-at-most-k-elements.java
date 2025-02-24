7cb24deb58b5626eef9d8bb87a380386d41074b7
Krishna K <krish1826s@gmail.com>
2025-02-24 22:00:04 +0530

    Time: 250 ms (0%), Space: 74.6 MB (0%) - LeetHub

--- 3764-maximum-sum-with-at-most-k-elements/3764-maximum-sum-with-at-most-k-elements.java
+++ 3764-maximum-sum-with-at-most-k-elements/3764-maximum-sum-with-at-most-k-elements.java
@@ -2,19 +2,16 @@ class Solution {
     public long maxSum(int[][] grid, int[] limits, int k) {
         long ans=0;
         PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
-        PriorityQueue<Integer> curr=new PriorityQueue<>((a,b)->b-a);
+        
         for(int i=0;i<grid.length;i++){
             int t=limits[i];
-            
+            PriorityQueue<Integer> curr=new PriorityQueue<>((a,b)->b-a);
             for(int j=0;j<grid[i].length;j++){
                 curr.offer(grid[i][j]);
             }
             while(t-->0){
                 pq.offer(curr.poll());
-            }   
-            while(!curr.isEmpty()){
-                curr.poll();
-            }         
+            }            
         }
                 
         while(k-->0){
