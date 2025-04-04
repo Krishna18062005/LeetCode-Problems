b3f0963ce4c810088881f69d917d2a14482ee5ff
Krishna K <krish1826s@gmail.com>
2025-04-04 10:11:49 +0530

    Time: 0 ms (100%), Space: 41.8 MB (29.49%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,13 +1,23 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int ans=0;
-        int n=citations.length;
-        for(int i=0;i<n;i++){
-            if(n-i<=citations[i])
-                return n-i;
-        }       
-        return ans;
-    }
+        int n = citations.length;
+        int arr[] =new int[n+1];
 
+        for(int c : citations){
+            if(c>n){
+                arr[n]++;
+            } else{
+                arr[c]++;
+            }
+        }
+
+        int count=0;
+        for(int i=n;i>=0;i--){
+            count = count + arr[i];
+            if(count>=i){
+                return i;
+            }
+        }
+        return 0;
+    }
 }
