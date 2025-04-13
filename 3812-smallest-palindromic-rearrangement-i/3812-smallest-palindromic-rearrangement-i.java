c23280496445712b44b631463677cdd01880c0e2
Krishna K <krish1826s@gmail.com>
2025-04-13 09:54:40 +0530

    Time: 37 ms (100%), Space: 46.3 MB (100%) - LeetHub

--- 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.java
@@ -1,31 +1,31 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        int[] freq=new int[26];
-        for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
         }
-        int ind=0;
-        StringBuilder st=new StringBuilder();
-        for(int i=0;i<26;i++){
-            if(freq[i]==0) continue;
-            else{
-                while(freq[i]>=2){
-                    freq[i]-=2;
-                    st.insert(st.length()-ind,(char)(i+'a'));
-                    st.insert(ind,(char)(i+'a'));
-                    ind++;
+
+        StringBuilder st = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0)
+                continue;
+            else {
+                while (freq[i] >= 2) {
+                    freq[i] -= 2;
+                    st.append((char) (i + 'a'));
                 }
             }
         }
-        for(int i=0;i<26;i++){
-            if(freq[i]==0){
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) {
                 continue;
-            }
-            else{
-                st.insert(ind,(char)(i+'a'));
+            } else {
+                char mid = ((char) (i + 'a'));
+                return st.toString() + mid + st.reverse().toString();
             }
         }
-        return st.toString();
-        
+        return st.toString() + st.reverse().toString();
+
     }
 }
