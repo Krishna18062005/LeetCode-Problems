c185f96cb0b4803136653193c2e9bd78ff3236f3
Krishna K <krish1826s@gmail.com>
2025-06-04 12:24:26 +0530

    Time: 1 ms (25.6%), Space: 42 MB (11.04%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.java
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -3,8 +3,9 @@ class Solution {
         int t=0;
         int a=0;
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='A') {t=0;a++;}
-            else if(s.charAt(i)=='L'){t++;if(t==3) return false;}
+            char ch=s.charAt(i);
+            if(ch=='A') {t=0;a++;}
+            else if(ch=='L'){t++;if(t==3) return false;}
             else{
                 t=0;
             }
