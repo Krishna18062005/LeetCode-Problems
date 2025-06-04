4ed8a6c13c6bd17255b74bbc4d911b5ec03afe4f
Krishna K <krish1826s@gmail.com>
2025-06-04 12:22:54 +0530

    Time: 1 ms (25.6%), Space: 41.4 MB (93.11%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.java
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -1,9 +1,13 @@
 class Solution {
     public boolean checkRecord(String s) {
-        if(s.contains("LLL")) return false;
+        int t=0;
         int a=0;
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='A') a++;
+            if(s.charAt(i)=='A') {t=0;a++;}
+            else if(s.charAt(i)=='L'){t++;if(t==3) return false;}
+            else{
+                t=0;
+            }
         }
         return a<2;
     }
