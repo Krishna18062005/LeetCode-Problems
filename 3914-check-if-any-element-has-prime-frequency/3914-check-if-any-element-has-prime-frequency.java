8c98d4f5bbdb8d0fdf8b7d4341515a30d2522c4f
Krishna K <krish1826s@gmail.com>
2025-06-22 14:54:11 +0530

    Time: 1 ms (98.7%), Space: 43 MB (50.57%) - LeetHub

--- 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.java
+++ 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.java
@@ -3,7 +3,7 @@ class Solution {
         if(n<=1) return false;
         if(n<=3) return true;
         
-        for(int i=2;i<n;i++){
+        for(int i=2;i*i<=n;i++){
             if(n%i==0) return false;
         }
         return true;
