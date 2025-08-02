52d5eaf91fea6414bc7fa744efe2bbef3664ee4a
Krishna K <krish1826s@gmail.com>
2025-08-02 21:50:08 +0530

    Time: 24 ms (85.1%), Space: 129.8 MB (63.75%) - LeetHub

--- 3766-maximum-median-sum-of-subsequences-of-size-3/3766-maximum-median-sum-of-subsequences-of-size-3.java
+++ 3766-maximum-median-sum-of-subsequences-of-size-3/3766-maximum-median-sum-of-subsequences-of-size-3.java
@@ -1,5 +1,9 @@
 class Solution {
-    public long maximumMedianSum(int[] nums) {
+    static{
+        for(int i=0;i<100;i++) maximumMedianSum(new int[2]);
+
+    }
+    public static long maximumMedianSum(int[] nums) {
         Arrays.sort(nums);
         int i=0,j=nums.length-2;
         long ans=0;
