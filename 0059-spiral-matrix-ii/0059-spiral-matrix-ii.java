8f98c2e959135d5a8d28783e38e3a466b333fe9c
Krishna K <krish1826s@gmail.com>
2025-08-05 13:43:35 +0530

    Time: 0 ms (100%), Space: 41.9 MB (8.82%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int [][]ans=new int[n][n];
+        int p=0;
+        for(int r=0;r<n;r++){
+            for(int c=r;c<n-r;c++) ans[r][c]=++p;
+            for(int c=r+1;c<n-r-1;c++) ans[c][n-r-1]=++p;
+            for(int c=n-r-1;c>r;c--) ans[n-r-1][c]=++p;
+            for(int c=n-r-1;c>r;c--) ans[c][r]=++p;
+        }
+        return ans;
+    }
+}
