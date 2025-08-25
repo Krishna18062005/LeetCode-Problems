eaf9023f3bf4d96f5b5640fd99679aa15b73ade1
Krishna K <krish1826s@gmail.com>
2025-08-25 15:55:31 +0530

    Time: 2 ms (99.41%), Space: 47.2 MB (56.76%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,36 +1,45 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-         int m = mat.length;
-        int n = mat[0].length;
-        int arr[] = new int[m * n];
-        int i = 0, j = 0, flag = 0;
-        for (int k = 0; k < m * n; k++) {
-            arr[k] = mat[i][j];
-            if (flag == 0) {
-                i--;
-                j++;
-                if (j > n - 1) {
-                    j = n - 1;
-                    i += 2;
-                    flag = 1;
-                } else if (i < 0) {
-                    i = 0;
-                    flag = 1;
-                }
-            } else {
-                i++;
-                j--;
-                if (i > m - 1) {
-                    i = m - 1;
-                    j += 2;
-                    flag = 0;
-                } else if (j < 0) {
-                    j = 0;
-                    flag = 0;
-                }
+       if (mat == null || mat.length == 0)
+            return new int[0];
 
+        int m = mat.length, n = mat[0].length;
+        int[] result = new int[m * n];
+        int i = 0, row = 0, col = 0;
+
+        while (i < m * n) {
+            // Go up-right when sum of indices is even
+            if ((row + col) % 2 == 0) {
+                while (row >= 0 && col < n) {
+                    result[i++] = mat[row][col];
+                    row--;
+                    col++;
+                }
+                // Move to next valid position
+                if (col == n) { // hit right border
+                    row += 2;
+                    col--;
+                } else {
+                    row++;
+                }
+            } 
+            // Go down-left when sum of indices is odd
+            else {
+                while (col >= 0 && row < m) {
+                    result[i++] = mat[row][col];
+                    row++;
+                    col--;
+                }
+                // Move to next valid position
+                if (row == m) { // hit bottom border
+                    col += 2;
+                    row--;
+                } else {
+                    col++;
+                }
             }
         }
-        return arr;
+
+        return result;
     }
 }
