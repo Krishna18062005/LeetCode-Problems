02b2f17d619796ee9d90364188d7af8274fc3932
Krishna K <krish1826s@gmail.com>
2025-09-14 16:12:38 +0530

    Time: 16 ms (100%), Space: 44.9 MB (100%) - LeetHub

---
+++ 4011-smallest-absent-positive-greater-than-average/4011-smallest-absent-positive-greater-than-average.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int smallestAbsent(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        int s=0;
+        for(int i:nums){
+            set.add(i);
+            s+=i;
+        }
+        s/=nums.length;
+        System.out.println(s);
+        s=Math.max(s,0);
+        s++;
+        while(s!=102){
+            if(!set.contains(s)) return s;
+            s++;
+        }
+        return 0;
+    }
+}
