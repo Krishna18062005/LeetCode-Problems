7b816ef48e9ab0a886ab04388745fc47a9a96694
Krishna K <krish1826s@gmail.com>
2025-09-14 22:21:50 +0530

    Time: 55 ms (21.8%), Space: 52.6 MB (23.22%) - LeetHub

---
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        Set<String> st = new HashSet<>(Arrays.asList(wordlist));
+        HashMap<String, String> mapc = new HashMap<>();
+        HashMap<String, String> mapv = new HashMap<>();
+        for (String i : wordlist) {
+            String lower = i.toLowerCase();
+            String vowel = lower.replaceAll("[aeiou]", "#");
+            mapc.putIfAbsent(lower, i);
+            mapv.putIfAbsent(vowel, i);
+        }
+        String[] ans = new String[queries.length];
+        int ind = 0;
+        for (String i : queries) {
+            if (st.contains(i)) {
+                ans[ind++] = i;
+            } else {
+                String lower = i.toLowerCase();
+                String vowel = lower.replaceAll("[aeiou]", "#");
+                if (mapc.containsKey(lower)) {
+                    ans[ind++] = mapc.get(lower);
+                } else if (mapv.containsKey(vowel))
+                    ans[ind++] = mapv.get(vowel);
+                else {
+                    ans[ind++] = "";
+                }
+            }
+        }
+        return ans;
+
+    }
+}
