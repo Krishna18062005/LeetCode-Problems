2570c142f5ebfa795add0f3d91e8b235d92f26b3
Krishna K <krish1826s@gmail.com>
2025-09-16 13:51:04 +0530

    Time: 87 ms (18.56%), Space: 63.1 MB (63.92%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
@@ -1,31 +1,32 @@
 class Solution {
     public List<Integer> replaceNonCoprimes(int[] nums) {
-        Stack<Integer> st=new Stack<>();
-        for(int i:nums){
-            if(st.empty()){
+        Stack<Integer> st = new Stack<>();
+        for (int i : nums) {
+            if (st.empty()) {
                 st.push(i);
-            }
-            else{
+            } else {
                 int cur = i;
-                //  keep merging until gcd == 1
+
                 while (!st.empty() && gcd(cur, st.peek()) > 1) {
                     int nu = st.pop();
-                    cur = (int)((long)nu * (cur / gcd(nu, cur))); // LCM
+                    cur = (int) ((long) nu * (cur / gcd(nu, cur))); // LCM
                 }
                 st.push(cur);
-                
+
             }
-            //System.out.println(st);
+
         }
-        List<Integer> ans=new ArrayList<>();
-        while(!st.empty()){
+        List<Integer> ans = new ArrayList<>();
+        while (!st.empty()) {
             ans.add(st.pop());
         }
         Collections.reverse(ans);
         return ans;
     }
-    int gcd(int a,int b){
-        if(b==0) return a;
-        return gcd(b,a%b);
+
+    int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
     }
 }
