c492a39ea250165eb04350c21a47433129b032fd
Krishna K <krish1826s@gmail.com>
2025-10-31 15:21:58 +0530

    Time: 102 ms (5.3%), Space: 125.2 MB (5.09%) - LeetHub

--- 2129-number-of-pairs-of-interchangeable-rectangles/2129-number-of-pairs-of-interchangeable-rectangles.java
+++ 2129-number-of-pairs-of-interchangeable-rectangles/2129-number-of-pairs-of-interchangeable-rectangles.java
@@ -7,20 +7,17 @@ class Solution {
             double r2 = (double) b[0] / b[1];
             return Double.compare(r1, r2);
         });
-
         long c = 0;
         int n = rectangles.length;
-
         for (int i = 0; i < n; i++) {
             int l = i, r = n - 1;
             double first = (double) rectangles[i][0] / rectangles[i][1];
-            int as = -1;
-
+            int is = -1;
             while (l <= r) {
                 int mid = (l + r) / 2;
                 double second = (double) rectangles[mid][0] / rectangles[mid][1];
                 if (first == second) {
-                    as = mid;
+                    is = mid;
                     l = mid + 1;
                 } else if (second > first) {
                     r = mid - 1;
@@ -28,11 +25,9 @@ class Solution {
                     l = mid + 1;
                 }
             }
-
-            if (as != -1)
-                c += (as - i);
+            if (is != -1)
+                c += (is - i);
         }
-
         return c;
     }
 }
