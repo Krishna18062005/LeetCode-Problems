d43d12a8b88fc3068f5c1a1af5c721c7a5002714
Krishna K <krish1826s@gmail.com>
2025-10-31 13:47:30 +0530

    Time: 490 ms (6.48%), Space: 47.6 MB (5.83%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -4,8 +4,8 @@ class Solution {
         for(int i=0;i<s.length();i++){
            
             if(s.substring(0,s.length()-i).equals(re.substring(i))) {
-                String a=new StringBuilder(re.substring(0,i)).reverse().toString();
-                return re.substring(0,i)+re.substring(i)+a;
+                
+                return re.substring(0,i)+re.substring(i)+new StringBuilder(re.substring(0,i)).reverse().toString();
             }
         }
         return "";
