dfb5c726c7fcda54b72f8e7bf6cc2524f9572be5
Krishna K <krish1826s@gmail.com>
2025-11-20 19:08:30 +0530

    Time: 1 ms (100%), Space: 43.3 MB (26.83%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,31 +1,30 @@
 class Solution {
     public int myAtoi(String s) {
-        long ans = 0;
-        int n = s.length();
-        if (n == 0) return 0;
+        long ans=0;
+       
+        if(s.length()<=0) return 0;
         
-        int i = 0;
-        while (i < n && s.charAt(i) == ' ') i++;
-        if (i == n) return 0;
         
-        int sign = 1;
-        if (s.charAt(i) == '-') {
-            sign = -1;
-            i++;
-        } else if (s.charAt(i) == '+') {
-            i++;
+        int i=0;
+        while(i<s.length()&&s.charAt(i)==' ') i++;
+        if(i==s.length()) return 0;
+        int m=1;
+        if(s.charAt(i)=='-') { m=-1;i++;}
+        else if(s.charAt(i)=='+') i++;
+        for(;i<s.length();i++){
+           char c=s.charAt(i);
+            
+            if(c<='9'&&c>='0'){
+                ans=ans*10+(c-'0');
+                if(m*ans<Integer.MIN_VALUE) return Integer.MIN_VALUE;
+                if(m*ans>Integer.MAX_VALUE) return Integer.MAX_VALUE; 
+
+            }
+            else{
+                break;
+            }
         }
         
-        while (i < n) {
-            char c = s.charAt(i);
-            if (c >= '0' && c <= '9') {
-                ans = ans * 10 + (c - '0');
-                if (sign == 1 && ans > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-                if (sign == -1 && -ans < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-            } else break;
-            i++;
-        }
-        
-        return (int)(sign * ans);
+        return (int)ans*m;
     }
 }
