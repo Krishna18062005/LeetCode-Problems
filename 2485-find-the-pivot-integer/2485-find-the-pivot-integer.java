589c3e3ccf1262af68198e782d137616bb704a10
Krishna K <krish1826s@gmail.com>
2025-12-27 09:37:52 +0530

    Time: 284 ms (5.42%), Space: 47 MB (5.07%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int pivotInteger(int n) {
+        if(n<=1) return n;
+        long s=(n*(n+1))/2;
+        int cs=0;
+        for(int i=1;i<=n;i++){
+            cs+=i;
+            System.out.println(cs+" "+s);
+            if(cs*2-i==s) return i;
+        }
+        return -1;
+    }
+}
